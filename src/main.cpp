30240ee9d465f33540c11b71d02c1d0b280d91dd
wxuennn <166488581+wxuennn@users.noreply.github.com>
2024-12-11 22:19:38 +0800

    Start user menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
+void CurrentDate();
+
 int main() {
-    cout << "Hello World!!!" << endl;
+
+    int choice;
+
+    do {
+
+        CurrentDate();
+
+        cout << "Monthly Budget Planner\n" 
+             << "1. Set Budget\n"
+             << "2. Make Expenses Record\n"
+             << "3. App Setting\n"
+             << "4. Exit\n"
+             << "Enter your choice: ";
+        cin  >> choice;
+
+    } while (choice != 4);
+
     return 0;
+
+}
+
+void CurrentDate() {
+
+    time_t t = time(0);
+    struct tm * now = localtime(&t);
+
+    cout << "Current Date: "
+         << (now->tm_year + 1900)
+         << '-'
+         << (now->tm_mon + 1)
+         << '-'
+         << now->tm_mday
+         << endl;
+
 }
