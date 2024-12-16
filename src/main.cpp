e767c706810278d70945d8217ee3396a83847575
YEAP BOON SHEN <boonshen1159@gmail.com>
2024-12-16 11:25:30 +0800

    "some extra part"

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ void displayBorder() {
 }
 void displayTitle(const string& title) {
     displayBorder();
-    cout << "|" << setw(20) << "" << title << setw(20) << "|\n";
+    cout << setw(20) << "" << title << setw(20) << "\n";
     displayBorder();
 }
 
